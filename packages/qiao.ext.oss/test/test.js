6ddf5f261d23636e1fc5f7e43e6d1c569c0cb86f
uikoo9 <uikoo9@qq.com>
2017-12-13 10:54:30 +0800

    add npmignore

--- packages/qiao.ext.oss/test/test.js
+++ packages/qiao.ext.oss/test/test.js
@@ -2,4 +2,11 @@ var qiaoExtOss 			= require('../libs/qiao.ext.oss.js');
 var qiaoExtOssConfig	= require('./config.json');
 
 var client = qiaoExtOss.client(qiaoExtOssConfig);
-console.log(client);
+
+// upload local file
+try{
+	var s = client.put('file', 'd:/test.js');
+	console.log(s);
+}catch(e){
+	console.log(e);
+}
