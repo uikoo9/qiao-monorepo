af5789288709afc887b1178571d9ec5d85f45b11
uikoo9 <uikoo9@qq.com>
2017-12-17 18:20:03 +0800

    upload folder async and sync

---
+++ packages/qiao.ext.oss/test/upload-folder-async.js
@@ -0,0 +1,14 @@
+var qiaoExtOss 			= require('../lib/qiao.ext.oss.js');
+var qiaoExtOssConfig	= require('./config.json');
+
+var client = qiaoExtOss.client(qiaoExtOssConfig);
+
+/**
+ * upload folder
+ * upload d:/test folder's files to your bucket's test folder
+ */
+qiaoExtOss.uploadFolder(client, 'test', 'd:/test', function(err, rs){
+	if(err) throw err;
+	
+	console.log(rs);
+});
---
+++ packages/qiao.ext.oss/test/upload-folder-sync.js
@@ -0,0 +1,22 @@
+var qiaoExtOss 			= require('../lib/qiao.ext.oss.js');
+var qiaoExtOssConfig	= require('./config.json');
+
+var client = qiaoExtOss.client(qiaoExtOssConfig);
+
+/**
+ * upload folder
+ * upload d:/test folder's files to your bucket's test folder
+ */
+var test = async function(){
+	try{
+		var rs1 = await qiaoExtOss.uploadFolderSync(client, 'test', 'd:/test');
+		console.log(rs1);
+
+		var rs2 = await qiaoExtOss.uploadFolderSync(client, 'test', 'd:/test');
+		console.log(rs2);
+	}catch(e){
+		console.log(e);
+	}
+};
+
+test();
