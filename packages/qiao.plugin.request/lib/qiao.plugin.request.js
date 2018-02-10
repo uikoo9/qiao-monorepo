4f244ef4b64c42018939b3dc2d465420b5cb7a12
uikoo9 <uikoo9@qq.com>
2018-02-10 16:44:49 +0800

    delete reqwest

--- packages/qiao.plugin.request/lib/qiao.plugin.request.js
+++ packages/qiao.plugin.request/lib/qiao.plugin.request.js
@@ -1,44 +1,6 @@
 'use strict';
 
-var reqwest = require('reqwest');
 var request = require('request');
-
-/**
- * ajax
- * 	options, https://www.npmjs.com/package/reqwest
- */
-exports.ajax = function(options){
-	if(typeof options != 'object'){
-		console.log('only support options type!');
-		return;
-	}
-	
-	reqwest(options);
-};
-
-/**
- * ajax sync
- * 	options, https://www.npmjs.com/package/reqwest
- * 	and you dont need success or error function in options
- * 	and you must babel first in brower
- */
-exports.ajaxSync = function(options){
-	if(typeof options != 'object'){
-		console.log('only support options type!');
-		return;
-	}
-	
-	return new Promise(function(resolve, reject){
-		options.success = function(resp){
-			resolve(resp);
-		};
-		options.error	= function(err){
-			reject(err);
-		};
-		
-		reqwest(options);
-	});
-};
 
 /**
  * get
