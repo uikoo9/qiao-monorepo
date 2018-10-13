145ac387943dfaad70c962de3006fba7dc7c5ee4
uikoo9 <uikoo9@qq.com>
2018-10-13 17:29:00 +0800

    add art-template

--- packages/qiao.plugin.coder/lib/qiao.plugin.coder.js
+++ packages/qiao.plugin.coder/lib/qiao.plugin.coder.js
@@ -1,9 +1,15 @@
 'use strict';
 
-var fs		= require('fs');
+var fs			= require('fs');
+var template	= require('art-template');
 
 /**
- * test
+ * genFile
+ * 	templateFile : template file path
+ * 	templateData : template data json file path
+ * 	destFile : dest file path
  */
-exports.test = function(){
+exports.genFile = function(templateFile, templateData, destFile){
+	var html = template(templateFile, templateData);
+	console.log(html);
 };
