d91f6c7e71718998225318d46c346660103b51da
uikoo9 <uikoo9@qq.com>
2018-10-17 20:50:59 +0800

    modify gen server code

--- packages/qiao.plugin.coder/test/gen-server-code.js
+++ packages/qiao.plugin.coder/test/gen-server-code.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var qiaoUtilString	= require('../util/qiao.util.string');
 var qiaoPluginCoder = require('../lib/qiao.plugin.coder');
-var qiaoPluginMysql = require('qiao.plugin.mysql');
 
 var test = async function(){
 	// vars
