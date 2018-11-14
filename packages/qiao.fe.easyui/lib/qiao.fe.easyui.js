36f31ad4fb2aeb57fcee9c0b5d5d73590e0de755
uikoo9 <uikoo9@qq.com>
2018-11-14 16:40:45 +0800

    add crud.init

--- packages/qiao.fe.easyui/lib/qiao.fe.easyui.js
+++ packages/qiao.fe.easyui/lib/qiao.fe.easyui.js
@@ -1,6 +1,39 @@
 'use strict';
 
 /**
+ * qiao.ajax
+ * 对$.ajax的封装
+ */
+exports.ajax = function(options, success, fail){
+	if(!options) return;
+	
+	var opt = $.extend({}, exports.ajax.options);
+	if(typeof options == 'string'){
+		opt.url = options;
+	}else{
+		$.extend(opt, options);
+	}
+	
+	$.ajax(opt).done(function(obj){
+		if(success) success(obj);
+	}).fail(function(e){
+		if(fail){
+			fail(e);
+		}else{
+			console.log('网络传输失败，请稍候重试！');
+		}
+	});
+};
+exports.ajax.options = {
+	url 		: '',
+	data 		: {},
+	type 		: 'post',
+	dataType	: 'json',
+	async 		: true,
+	crossDomain	: false
+};
+
+/**
  * alert
  * 	options, msg
  * 	fn, callback
@@ -60,6 +93,73 @@ exports.addTab = function(url, title){
  */
 exports.crud = {};
 
+exports.crud.init = function(url, cols){
+	if(!url || !cols){
+		console.log('error: qiao.easyui.curd.init need url and cols');
+		return;
+	}
+	
+	var $this = $('#datagrid'); 
+	if(!$this){
+		console.log('error:not found datagrid by #datagrid');
+		return;
+	}
+	
+	$this.datagrid({
+	    url			: url + '/list',
+	    idField		: 'id',
+	    loadMsg		: '加载中，请稍候...',
+	    emptyMsg	: '暂无数据',
+	    fitColumns	: true,
+	    pagination	: true,
+	    rownumbers	: true,
+	    pageNumber	: 1,
+	    pageSize	: 10,
+	    pageList	: [10,50,100],
+	    columns 	: cols,
+	    loadFilter	: function(data){
+	    	return data.obj;
+	    },
+	    toolbar: [{
+	    	text	: '添加',
+			iconCls	: 'icon-add',
+			handler	: function(){
+				exports.crud.save({
+					title	: '添加',
+					editUrl	: url + '/edit',
+					saveUrl	: url + '/save',
+					width	: 400,
+					height	: 200
+				});
+			}
+	    },'-',{
+	    	text	: '修改',
+	    	iconCls	: 'icon-edit',
+	    	handler	: function(){
+	    		exports.crud.edit({
+					title	: '修改',
+					editUrl	: url + '/edit',
+					saveUrl	: url + '/save',
+					width	: 400,
+					height	: 200
+				});
+			}
+		},'-',{
+			text	: '删除',
+			iconCls	: 'icon-no',
+			handler	: function(){
+				exports.crud.del(url + '/del');
+			}
+		},'-',{
+			text	: '搜索',
+			iconCls	: 'icon-search',
+			handler	: function(){
+				alert(1);
+			}
+		}]
+	});
+};
+
 /**
  * crud.edit
  * 	options.id
@@ -72,7 +172,7 @@ exports.crud = {};
 exports.crud.edit = function(options){
 	var $this = $('#datagrid'); 
 	if(!$this){
-		console.log('error:not found datagrid by #dg');
+		console.log('error:not found datagrid by #datagrid');
 		return;
 	}
 	
@@ -177,37 +277,4 @@ exports.crud.del = function(url){
 			exports.alert('请求' + url + '失败！');
 		}
 	});
-};
-
-/**
- * qiao.ajax
- * 对$.ajax的封装
- */
-exports.ajax = function(options, success, fail){
-	if(!options) return;
-	
-	var opt = $.extend({}, exports.ajax.options);
-	if(typeof options == 'string'){
-		opt.url = options;
-	}else{
-		$.extend(opt, options);
-	}
-	
-	$.ajax(opt).done(function(obj){
-		if(success) success(obj);
-	}).fail(function(e){
-		if(fail){
-			fail(e);
-		}else{
-			console.log('网络传输失败，请稍候重试！');
-		}
-	});
-};
-exports.ajax.options = {
-	url 		: '',
-	data 		: {},
-	type 		: 'post',
-	dataType	: 'json',
-	async 		: true,
-	crossDomain	: false
 };
