393acb7d759c27d6b59aec85dec7e018bcc95fc4
uikoo9 <uikoo9@qq.com>
2018-11-19 17:09:18 +0800

    manage-db

--- packages/qiao.plugin.coder/codes/01/_coder_01.js
+++ packages/qiao.plugin.coder/codes/01/_coder_01.js
@@ -54,9 +54,9 @@ exports.gen = async function(destFolder, tableName){
 	
 	// gen server code
 	genController(destFolder, tableName1, className1, data);
-	genModel(destFolder, tableName1, className1, data);
 	genService(destFolder, tableName1, className1, data);
-	genSql(destFolder, data);
+	genModel(destFolder, tableName1, className1, data);
+	genSql(destFolder, tableName1, tableName2, data);
 	
 	// gen webroot code
 //	genHtml(destFolder, tableName1, tableName2, data);
@@ -78,28 +78,28 @@ function notIn(s){
 // gen controller
 function genController(destFolder, tableName1, className1, data){
 	var controllerTemp 	= path.resolve(__dirname, './server/controller.art');
-	var controllerDest	= path.resolve(destFolder, './server/manage/' + tableName1 + '/controller/' + className1 + 'Controller.js');
+	var controllerDest	= path.resolve(destFolder, './server/manage-db/' + tableName1 + '/controller/' + className1 + 'Controller.js');
 	qiao.coder.genFileByData(controllerTemp, data, controllerDest);
-}
-
-// gen model
-function genModel(destFolder, tableName1, className1, data){
-	var modelTemp 	= path.resolve(__dirname, './server/model.art');
-	var modelDest	= path.resolve(destFolder, './server/manage/' + tableName1 + '/model/' + className1 + 'Model.js');
-	qiao.coder.genFileByData(modelTemp, data, modelDest);
 }
 
 // gen service
 function genService(destFolder, tableName1, className1, data){
 	var serviceTemp = path.resolve(__dirname, './server/service.art');
-	var serviceDest	= path.resolve(destFolder, './server/manage/' + tableName1 + '/service/' + className1 + 'Service.js');
+	var serviceDest	= path.resolve(destFolder, './server/manage-db/' + tableName1 + '/service/' + className1 + 'Service.js');
 	qiao.coder.genFileByData(serviceTemp, data, serviceDest);
 }
 
+// gen model
+function genModel(destFolder, tableName1, className1, data){
+	var modelTemp 	= path.resolve(__dirname, './server/model.art');
+	var modelDest	= path.resolve(destFolder, './server/manage-db/' + tableName1 + '/model/' + className1 + 'Model.js');
+	qiao.coder.genFileByData(modelTemp, data, modelDest);
+}
+
 // gen sql
-function genSql(destFolder, data){
+function genSql(destFolder, tableName1, tableName2, data){
 	var sqlTemp = path.resolve(__dirname, './server/sql.art');
-	var sqlDest	= path.resolve(destFolder, './server/manage/manage-sql.json');
+	var sqlDest	= path.resolve(destFolder, './server/manage-db/' + tableName1 + '/model/' + tableName1 + '-' + tableName2 + '-sql.json');
 	qiao.coder.genFileByData(sqlTemp, data, sqlDest);
 }
 
