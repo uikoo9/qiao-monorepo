153c256d544d38267a77e947d5db2f4fbe5bf4da
uikoo9 <uikoo9@qq.com>
2020-03-12 21:04:58 +0800

    add ping

--- packages/qiao.plugin.request/lib/qiao.plugin.request.js
+++ packages/qiao.plugin.request/lib/qiao.plugin.request.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var fs = require('fs');
+var fs 		= require('fs');
+var ping 	= require('ping');
 
 /**
  * reuqest
@@ -189,5 +190,22 @@ exports.download = function(url, path){
 				resolve();
 			}));
 		});
+	});
+};
+
+/**
+ * ping
+ * 	host
+ * 	options,https://www.npmjs.com/package/ping
+ */
+exports.ping = function(host, options){
+	if(!host){
+		console.log('need host');
+		return;
+	}
+
+	var opt = options || {timeout:10};
+	return new Promise(function(resolve, reject){
+		ping.promise.probe(host, opt).then(function(res){return resolve(res);});
 	});
 };
