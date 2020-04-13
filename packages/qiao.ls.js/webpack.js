9e8ce98b6745a1816332dbb815784c61b9713c7d
uikoo9 <uikoo9@qq.com>
2020-04-14 00:23:03 +0800

    cache

--- packages/qiao.ls.js/webpack.js
+++ packages/qiao.ls.js/webpack.js
@@ -4,6 +4,7 @@ module.exports = {
         'get-item' : './test/get-item.js',
         'remove-item' : './test/remove-item.js',
         'set-item' : './test/set-item.js',
+        'cache' : './test/cache.js',
     },
     output  : {
         filename        : '[name].js',
