500a684a07077eab93c40c30a135e91f07a1f7f2
uikoo9 <uikoo9@qq.com>
2020-04-14 00:52:13 +0800

    add cache and ls

--- packages/qiao.indexeddb.js/test/cache.js
+++ packages/qiao.indexeddb.js/test/cache.js
@@ -2,8 +2,21 @@
 
 var qls = require('../lib/qiao.ls.js');
 
-qls.setCache('cache.group.role', 'team-123', false);
-qls.setCache('cache.group.role', 'team-123', true);
-console.log(qls.getCache('cache.group.role', 'team-123'));
-qls.removeCache('cache.group.role', 'team-123');
-qls.clearCache('cache.group.role');
+// qls.setCache('cache.group.role', 'team-123', false);
+// qls.setCache('cache.group.role', 'team-123', true);
+// console.log(qls.getCache('cache.group.role', 'team-123'));
+// qls.removeCache('cache.group.role', 'team-123');
+// qls.clearCache('cache.group.role');
+
+/**
+ * cache
+ *  cache('name', null);
+ *  cache('name', 'key', null);
+ *  cache('name', 'key');
+ *  cache('name', 'key', value, exp);
+ */
+qls.cache('cache.group.role', 'team-123', false);
+qls.cache('cache.group.role', 'team-123', true);
+console.log(qls.cache('cache.group.role', 'team-123'));
+qls.cache('cache.group.role', 'team-123', null);
+qls.cache('cache.group.role', null);
