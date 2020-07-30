e8dc7e958f2303a01ef015b22c044972b7a471b9
uikoo9 <uikoo9@qq.com>
2020-07-31 00:16:07 +0800

    ncu

--- packages/qiao.indexeddb.js/webpack.js
+++ packages/qiao.indexeddb.js/webpack.js
@@ -1,16 +1,16 @@
 module.exports = {
     mode    : 'production',
     entry   : {
-        'open-db' : './test/open-db.js',
-        'del-db' : './test/del-db.js',
-        'create-table' : './test/create-table.js',
-        'del-table' : './test/del-table.js',
-        'add' : './test/add.js',
-        'get' : './test/get.js',
-        'put' : './test/put.js',
-        'save' : './test/save.js',
-        'del' : './test/del.js',
-        'clear' : './test/clear.js'
+        'open-db'       : './test/open-db.js',
+        'del-db'        : './test/del-db.js',
+        'create-table'  : './test/create-table.js',
+        'del-table'     : './test/del-table.js',
+        'add'           : './test/add.js',
+        'get'           : './test/get.js',
+        'put'           : './test/put.js',
+        'save'          : './test/save.js',
+        'del'           : './test/del.js',
+        'clear'         : './test/clear.js'
     },
     output  : {
         filename        : '[name].js',
