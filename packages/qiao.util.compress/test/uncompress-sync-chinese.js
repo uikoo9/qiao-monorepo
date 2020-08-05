c34f1e1336a975837a0f7cd1ce760b5b733bd68a
uikoo9 <uikoo9@qq.com>
2020-08-05 11:39:46 +0800

    uncompress ok

---
+++ packages/qiao.util.compress/test/uncompress-sync-chinese.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var path    = require('path');
+var q       = require('../qiao.util.compress');
+
+var test = async function(){
+    var sourceFile  = path.resolve(__dirname, '../files_in/uncompress-file-中文.zip');
+    var destPath    = path.resolve(__dirname, '../files_out/');
+
+    try{
+        await q.uncompressSync('zip', sourceFile, destPath);
+        console.log(`uncompress file: ${sourceFile} success, to ${destPath}`);
+    }catch(e){
+        console.log(`uncompress file: ${sourceFile} fail: ${e}`);
+    }
+};
+
+test();
