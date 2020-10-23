8dcc4e5bd1148030bdeafb1188936f292eda4dc7
uikoo9 <uikoo9@qq.com>
2020-10-23 16:55:05 +0800

    get

--- packages/qiao.indexeddb.js/test/get.js
+++ packages/qiao.indexeddb.js/test/get.js
@@ -3,32 +3,14 @@
 var qdb = require('../lib/qiao.indexeddb.js');
 
 var test = async function(){
-	var databaseName = 'db_test';
-	var version = 1;
-	var tables = [{
-		name : 't_test1',
-		key : 'id',
-		index : {
-			name : 'name',
-			unique : false
-		}
-	},{
-		name : 't_test2',
-		key : 'auto',
-		index : {
-			name : 'email',
-			unique : true
-		}
-	}];
+	var databaseName 	= 'db_test';
+	var tableName		= 't_test1';
+	var version 		= 2;
 
 	try{
 		var db 	= await qdb.openDB(databaseName, version);
-		var res = qdb.createTable(db, tables);
-		console.log(res);
-
-		var tableName = 't_test1';
-		var data = { id: 1, name: '张三', age: 24, email: 'zhangsan@example.com' };
-		await qdb.save(db, tableName, data);
+		var s 	= await qdb.get(db, tableName, 1);
+		console.log(s);
 	}catch(e){
 		console.log(e);
 	}
