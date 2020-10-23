47a2ae078ec0f4f0eb4fb5f603b14a3e74e5b5f4
uikoo9 <uikoo9@qq.com>
2020-10-23 16:57:37 +0800

    del

--- packages/qiao.indexeddb.js/test/del.js
+++ packages/qiao.indexeddb.js/test/del.js
@@ -2,41 +2,17 @@
 
 var qdb = require('../lib/qiao.indexeddb.js');
 
-var test = function(){
-	var databaseName = 'db_test';
-	var version = 1;
-	var tables = [{
-		name : 't_test1',
-		key : 'id',
-		index : {
-			name : 'name',
-			unique : false
-		}
-	},{
-		name : 't_test2',
-		key : 'auto',
-		index : {
-			name : 'email',
-			unique : true
-		}
-	}];
+var test = async function(){
+	var databaseName 	= 'db_test';
+	var tableName		= 't_test1';
+	var version 		= 2;
 
-	qdb.openDB(databaseName, version, function(db){
-		var res = qdb.createTable(db, tables);
-		console.log(res);
-	});
-
-	qdb.openDB(databaseName, version, null, function(db){
-		var tableName = 't_test1';
-		var data = { id: 1, name: '张三', age: 24, email: 'zhangsan@example.com' };
-		qdb.add(db, tableName, data, function(r){
-			if(!r) return;
-
-			qdb.del(db, tableName, 1, function(rr){
-				console.log(rr);
-			});
-		});
-	});
+	try{
+		var db 	= await qdb.openDB(databaseName, version);
+		await qdb.del(db, tableName, 2);
+	}catch(e){
+		console.log(e);
+	}
 };
 
 test();
