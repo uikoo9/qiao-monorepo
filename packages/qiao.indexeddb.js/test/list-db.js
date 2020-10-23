efd83820b01edf51360ee1f00525b74dd10508f1
uikoo9 <uikoo9@qq.com>
2020-10-23 14:10:35 +0800

    list db

--- packages/qiao.indexeddb.js/test/list-db.js
+++ packages/qiao.indexeddb.js/test/list-db.js
@@ -2,10 +2,9 @@
 
 var qdb = require('../lib/qiao.indexeddb.js');
 
-var test = function(){
-	qdb.listDB(function(dbs){
-		console.log(dbs);
-	});
+var test = async function(){
+	var dbs = await qdb.listDB();
+	console.log(dbs);
 };
 
 test();
