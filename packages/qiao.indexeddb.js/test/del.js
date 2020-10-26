a4e0c1c8b0b139d68db145287fd0ce8838179ff4
uikoo9 <uikoo9@qq.com>
2020-10-26 17:54:55 +0800

    del del version

--- packages/qiao.indexeddb.js/test/del.js
+++ packages/qiao.indexeddb.js/test/del.js
@@ -5,10 +5,9 @@ var qdb = require('../lib/qiao.indexeddb.js');
 var test = async function(){
 	var databaseName 	= 'db_test';
 	var tableName		= 't_test1';
-	var version 		= 2;
 
 	try{
-		var db = await qdb.openDB(databaseName, version);
+		var db = await qdb.openDB(databaseName);
 		await qdb.del(db, tableName, 2);
 	}catch(e){
 		console.log(e);
