629f9027f32f5c87cba3f99910e824443dc10624
uikoo9 <uikoo9@qq.com>
2021-06-04 23:51:05 +0800

    add cli

---
+++ packages/multi-ncu/bin/multi-ncu.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+'use strict';
+
+// qiao
+var qiao 	= {};
+qiao.cli 	= require('qiao.plugin.cli');
+
+// package
+var pkg = require('../package.json'); 
+
+// multi-ncu
+var mncu = require('../lib/multi-ncu.js');
+-
+// cmd for version
+qiao.cli.cmd
+    .version(pkg.version, '-v, --version')
+    .usage('<command> [options]');
+
+// cmd for run
+qiao.cli.cmd
+    .command('run [destPath]')
+    .usage('[destPath]')
+    .description('multi ncu in path')
+    .action(run);
+
+// run
+function run(destPath){
+    var _path = destPath || '.';
+    mncu.multiNCU(_path);
+}
+
+// parse
+qiao.cli.cmd.parse(process.argv);
