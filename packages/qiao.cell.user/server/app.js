487c7e844de632bb03d0f4a01f393d5e7ffe243a
uikoo9 <uikoo9@qq.com>
2021-08-03 17:36:00 +0800

    update code

--- packages/qiao.cell.user/server/app.js
+++ packages/qiao.cell.user/server/app.js
@@ -1,8 +1,11 @@
-'use strict';
+'user strict';
 
 // vars
 var root	= __dirname;
 var port	= require('./config/config.json').port;
+
+// require
+var qiao 	= require('qiao.util.all');
 
 // express
 var express 		= require('express');
@@ -16,8 +19,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text({type:'text/xml'}));
 
+// auth
+app.use(require('./middleware/qiao.mid.js').crossDomain);
+app.use(require('./middleware/qiao.mid.js').auth);
+
 // controller
-var serverFiles = require('qiao.util.file').lsdir(root + '/');
+var serverFiles = qiao.file.lsdir(root + '/');
 for(var i=0; i<serverFiles.files.length; i++){
 	var file = serverFiles.files[i].path + serverFiles.files[i].name;
 	if(/Controller\.js$/.test(file)) require(file)(app);
