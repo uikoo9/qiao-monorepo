2c24e93510635d6789355eb1fb9e6bcdc1d18970
uikoo9 <uikoo9@qq.com>
2021-08-23 20:11:20 +0800

    del qiao.cell.user.js

--- packages/qiao.cell.user/index.js
+++ packages/qiao.cell.user/index.js
@@ -1,1 +1,29 @@
-module.exports = require('./lib/qiao.cell.user.js');
+'use strict';
+
+/**
+ * init
+ * @param {*} app express app
+ */
+exports.init = function(app){
+	// check config
+	if(!global.config){
+		console.log('need global.config');
+		return;
+	}
+
+	// check app
+	if(!app){
+		console.log('need express app');
+		return;
+	}
+
+	// init controller
+	exports.ucenterUserController(app);
+};
+
+/**
+ * ucenter user
+ */
+exports.ucenterUserController 	= require('./lib/ucenter/controller/UcenterUserController.js');
+exports.ucenterUserService		= require('./lib/ucenter/service/UcenterUserService.js');
+exports.ucenterUserModel		= require('./lib/ucenter/model/UcenterUserModel.js');
