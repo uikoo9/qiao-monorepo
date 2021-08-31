374203cb8aa38736a74f9b36990094a1facb0c08
uikoo9 <uikoo9@qq.com>
2021-08-31 16:18:24 +0800

    add sql

---
+++ packages/dishi-monorepo/packages/dishi-server-todo/_sql/t_todo_group.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `t_todo_group`;
+
+CREATE TABLE `t_todo_group` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `ucenter_user_id` int(10) NOT NULL COMMENT 'userid',
+  `todo_group_name` varchar(200) NOT NULL COMMENT 'todo分组名称',
+  `todo_group_order` varchar(20) NOT NULL COMMENT 'todo分组排序',
+  `cdate` datetime NOT NULL COMMENT '创建时间',
+  `cuser_id` int(10) NOT NULL COMMENT '创建人id',
+  `cuser_name` varchar(200) NOT NULL COMMENT '创建人姓名',
+  `udate` datetime NOT NULL COMMENT '更新时间',
+  `uuser_id` int(10) NOT NULL COMMENT '更新人id',
+  `uuser_name` varchar(200) NOT NULL COMMENT '更新人姓名',
+  `del_tag` char(1) NOT NULL COMMENT '是否删除，0否，1是',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
