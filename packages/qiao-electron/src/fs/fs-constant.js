f05587a5237058ee8f07c713842ba13558bb3c35
uikoo9 <uikoo9@qq.com>
2022-04-20 14:54:17 +0800

    feat(qiao-eletron): fs rename

--- packages/qiao-electron/src/fs/fs-constant.js
+++ packages/qiao-electron/src/fs/fs-constant.js
@@ -3,5 +3,6 @@
 /**
  * fs constant
  */
+export const IPC_FS_RENAME      = 'ipc-fs-rename';
 export const IPC_FS_GET_TREE    = 'ipc-fs-get-tree';
 export const IPC_FS_READ_FILE   = 'ipc-fs-read-file';
--- packages/qiao-electron/src/fs/fs-ipc.js
+++ packages/qiao-electron/src/fs/fs-ipc.js
@@ -4,23 +4,30 @@
 import { ipcMain } from 'electron';
 
 // q
-import { lstree, readFile } from 'qiao-file';
+import { mv, lstree, readFile } from 'qiao-file';
 
 // const
-import { IPC_FS_GET_TREE, IPC_FS_READ_FILE } from './fs-constant.js';
+import { IPC_FS_RENAME, IPC_FS_GET_TREE, IPC_FS_READ_FILE } from './fs-constant.js';
 
 /**
  * fsIPCInit
  */
 export const fsIPCInit = () => {
-  // ipc ls get tree
+  // ipc fs rename
+  ipcMain.handle(IPC_FS_RENAME, (event, oldPath, newPath) => {
+    if(!oldPath || !newPath) return;
+
+    return mv(oldPath, newPath);
+  });
+  
+  // ipc fs get tree
   ipcMain.handle(IPC_FS_GET_TREE, (event, dir, ignores) => {
     if(!dir) return;
 
     return lstree(dir, ignores);
   });
 
-  // ipc ls read file
+  // ipc fs read file
   ipcMain.handle(IPC_FS_READ_FILE, (event, filePath) => {
     if(!filePath) return;
 
