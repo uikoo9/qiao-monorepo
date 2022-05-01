c251330d9f62bc9e565d50eec360070535da82ba
uikoo9 <uikoo9@qq.com>
2022-05-01 22:16:09 +0800

    feat(qiao-electron-cli): qe pack mac

--- packages/qiao-electron-cli/lib/pack-mac.js
+++ packages/qiao-electron-cli/lib/pack-mac.js
@@ -8,12 +8,22 @@ var packager = require('electron-packager');
  */
 module.exports = async function(config){
     // check
-    if(!config)         throw new Error('need config params');
-    if(!config.dir)     throw new Error('need config.dir params');
-    if(!config.out)     throw new Error('need config.out params');
-    if(!config.name)    throw new Error('need config.name params');
-    if(!config.dir)     throw new Error('need config.dir params');
+    if(!config)                         throw new Error('need config params');
+    if(!config.appConfig)               throw new Error('need config.appConfig params');
+    if(!config.appConfig.appName)       throw new Error('need config.appConfig.appName params');
+    if(!config.distConfig)              throw new Error('need config.distConfig params');
+    if(!config.distConfig.distPath)     throw new Error('need config.distConfig.distPath params');
+    if(!config.packmacConfig)           throw new Error('need config.packmacConfig params');
+    if(!config.packmacConfig.out)       throw new Error('need config.packmacConfig.out params');
+
+    // options
+    var options     = config.packmacConfig;
+    options.dir     = config.distConfig.distPath;
+    options.name    = config.appConfig.appName;
+    if(config.appConfig.appVersion)     options.appVersion  = config.appConfig.appVersion;
+    if(config.appConfig.appIconPath)    options.icon        = config.appConfig.appIconPath;
+    if(config.appConfig.appCopyright)   options.appCopyright= config.appConfig.appCopyright;
 
     // packager
-    return await packager(config);
+    return await packager(options);
 };
