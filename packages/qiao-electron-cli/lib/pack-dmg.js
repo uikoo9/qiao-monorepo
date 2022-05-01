288ca3aa8e523a6a15a3113360577a5a536607c4
uikoo9 <uikoo9@qq.com>
2022-05-01 22:50:37 +0800

    feat(qiao-electron-cli): qe pack dmg

--- packages/qiao-electron-cli/lib/pack-dmg.js
+++ packages/qiao-electron-cli/lib/pack-dmg.js
@@ -1,3 +1,6 @@
+// path
+var path = require('path');
+
 // electron installer dmg
 var electronDMG = require('electron-installer-dmg');
 
@@ -7,15 +10,33 @@ var electronDMG = require('electron-installer-dmg');
  * @returns 
  */
 module.exports = async function(config){
-    // check
-    if(!config)         throw new Error('need config params');
-    if(!config.out)     throw new Error('need config.out params');
-    if(!config.name)    throw new Error('need config.name params');
-    if(!config.appPath) throw new Error('need config.appPath params');
+    // check config
+    if(!config) throw new Error('need config params');
+
+    // check app config
+    var appConfig = config.appConfig;
+    if(!appConfig)              throw new Error('need config.appConfig params');
+    if(!appConfig.appEnv)       throw new Error('need config.appConfig.appEnv params');
+    if(!appConfig.appName)      throw new Error('need config.appConfig.appName params');
+    if(!appConfig.appVersion)   throw new Error('need config.appConfig.appVersion params');
+    if(!appConfig.appIconPath)  throw new Error('need config.appConfig.appIconPath params');
+
+    // check packmac config
+    var packmacConfig = config.packmacConfig;
+    if(!packmacConfig) throw new Error('need config.packmacConfig params');
+
+    // check packdmg config
+    var packdmgConfig = config.packdmgConfig;
+    if(!packdmgConfig) throw new Error('need config.packdmgConfig params');
+
+    // options
+    var options = config.packdmgConfig;
+    options.name = `${appConfig.appName}-${appConfig.appEnv}-${appConfig.appVersion}`;
+    options.icon = appConfig.appIconPath;
 
     // dmg
     return new Promise(function(resolve, reject){
-        electronDMG(config, function(err){
+        electronDMG(options, function(err){
             return err ? reject(err) : resolve();
         });
     });
