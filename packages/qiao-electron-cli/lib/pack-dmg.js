f1ad383b599fd5830357e7605dae98e92a066b40
uikoo9 <uikoo9@qq.com>
2022-05-02 16:49:15 +0800

    feat(qiao-electron-cli): pack dmg

--- packages/qiao-electron-cli/lib/pack-dmg.js
+++ packages/qiao-electron-cli/lib/pack-dmg.js
@@ -1,3 +1,8 @@
+'use strict';
+
+// path
+var path = require('path');
+
 // electron installer dmg
 var electronDMG = require('electron-installer-dmg');
 
@@ -11,28 +16,36 @@ module.exports = async function(config){
     if(!config) throw new Error('need config params');
 
     // vars
+    var outPath         = config.outPath;
+    var arch            = config.arch;
     var appEnv          = config.appEnv;
     var appName         = config.appName;
-    var appPath         = config.appPath;
     var appVersion      = config.appVersion;
     var appIconPath     = config.appIconPath;
-    var dmgName         = config.dmgName;
     var dmgIconSize     = config.dmgIconSize;
     var dmgBackground   = config.dmgBackground;
-    var dmgContents     = config.dmgContents;
-    var dmgOutPath      = config.dmgOutPath;
 
     // check vars
+    if(!outPath)        throw new Error('need config.outPath params');
+    if(!arch)           throw new Error('need config.arch params');
     if(!appEnv)         throw new Error('need config.appEnv params');
     if(!appName)        throw new Error('need config.appName params');
-    if(!appPath)        throw new Error('need config.appPath params');
     if(!appVersion)     throw new Error('need config.appVersion params');
     if(!appIconPath)    throw new Error('need config.appIconPath params');
-    if(!dmgName)        throw new Error('need config.dmgName params');
     if(!dmgIconSize)    throw new Error('need config.dmgIconSize params');
     if(!dmgBackground)  throw new Error('need config.dmgBackground params');
-    if(!dmgContents)    throw new Error('need config.dmgContents params');
-    if(!dmgOutPath)     throw new Error('need config.dmgOutPath params');
+
+    // other vars
+    var root            = process.cwd();
+    var appPath         = path.resolve(root, `${outPath}/${appName}-darwin-${arch}/${appName}.app`);
+    var dmgName         = `${appName}-${appEnv}-${appVersion}`;
+    var dmgOutPath      = path.resolve(root, `${outPath}/dmg`);
+    var dmgContents     = () => {
+        return [ 
+            { x: 520, y: 200, type: 'link', path: '/Applications' },
+            { x: 120, y: 200, type: 'file', path: appPath } 
+        ];
+    };
 
     // options
     var options = {
