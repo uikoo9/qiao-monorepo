76f97db15ff7086868f97c2bd49ee49228838e04
uikoo9 <uikoo9@qq.com>
2022-05-05 16:40:40 +0800

    feat(qiao-webpack): performance

---
+++ packages/qiao-webpack/config/webpack-performance.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * webpack performance
+ *  https://webpack.docschina.org/configuration/performance/
+ */
+module.exports = {
+    hints               : 'warning',
+    maxAssetSize        : 250000,
+    maxEntrypointSize   : 250000,
+    assetFilter         : function(assetFilename){
+        return !/\.map$/.test(assetFilename);
+    },
+};
--- packages/qiao-webpack/config/webpack.js
+++ packages/qiao-webpack/config/webpack.js
@@ -15,6 +15,9 @@ var webpack_dev_plugins = require('./webpack-dev-plugins.js');
 var webpack_build_options = require('./webpack-build-options.js');
 var webpack_build_plugins = require('./webpack-build-plugins.js');
 
+// webpack performance
+var webpack_performance = require('./webpack-performance.js');
+
 /**
  * webpack build config
  */
@@ -24,11 +27,13 @@ exports.build = function(configPath, target, isAnalyzer){
     if (!qiaoWebpack) return;
 
     // opt
-    var opt     = Object.assign({}, webpack_build_options);
-    opt.entry   = qiaoWebpack.entry;
-    opt.output  = qiaoWebpack.output;
-    opt.resolve = qiaoWebpack.resolve;
-    opt.plugins = webpack_build_plugins(qiaoWebpack.plugins, isAnalyzer);
+    var opt             = Object.assign({}, webpack_build_options);
+    opt.entry           = qiaoWebpack.entry;
+    opt.output          = qiaoWebpack.output;
+    opt.resolve         = qiaoWebpack.resolve;
+    opt.plugins         = webpack_build_plugins(qiaoWebpack.plugins, isAnalyzer);
+    opt.performance     = qiaoWebpack.performance || webpack_performance;
+    opt.optimization    = qiaoWebpack.optimization;
 
     // target
     if(target) opt.target = target;
@@ -46,12 +51,14 @@ exports.build = function(configPath, target, isAnalyzer){
     if (!qiaoWebpack) return;
 
     // opt
-    var opt         = Object.assign({}, webpack_dev_options);
-    opt.entry       = qiaoWebpack.entry;
-    opt.output      = qiaoWebpack.output;
-    opt.resolve     = qiaoWebpack.resolve;
-    opt.devServer   = qiaoWebpack.devServer;
-    opt.plugins     = webpack_dev_plugins(qiaoWebpack.plugins);
+    var opt             = Object.assign({}, webpack_dev_options);
+    opt.entry           = qiaoWebpack.entry;
+    opt.output          = qiaoWebpack.output;
+    opt.resolve         = qiaoWebpack.resolve;
+    opt.devServer       = qiaoWebpack.devServer;
+    opt.plugins         = webpack_dev_plugins(qiaoWebpack.plugins);
+    opt.performance     = qiaoWebpack.performance || webpack_performance;
+    opt.optimization    = qiaoWebpack.optimization;
 
     // target
     if(target) opt.target = target;
