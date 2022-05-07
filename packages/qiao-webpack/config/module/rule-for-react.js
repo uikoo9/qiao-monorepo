c0287aa65fb7b959c66216c1913710df5a147b6a
uikoo9 <uikoo9@qq.com>
2022-05-07 21:30:28 +0800

    feat(qiao-webpack): babel-plugin-import

--- packages/qiao-webpack/config/module/rule-for-react.js
+++ packages/qiao-webpack/config/module/rule-for-react.js
@@ -2,11 +2,25 @@
 
 /**
  * rule for react
- *  npm i -D babel-loader @babel/core @babel/preset-react
+ *  npm i -D babel-loader @babel/core @babel/preset-react babel-plugin-import
  */
 module.exports = {
     test    : /\.(js|jsx)$/,
     loader  : 'babel-loader',
     // exclude : /node_modules/,
-    options : { presets: ["@babel/preset-react"] }
+    options : { 
+        presets: [
+            '@babel/preset-react'
+        ],
+        plugins: [
+            [
+                'import',
+                {
+                    libraryName       : 'antd',
+                    libraryDirectory  : 'es',
+                    style             : 'css'
+                }
+            ]
+        ]
+    }
 };
