2064b7889d36a1f832516dfab02a117667af6ea2
uikoo9 <uikoo9@qq.com>
2022-05-08 19:58:11 +0800

    feat(qiao-webpack): stats

--- packages/qiao-webpack/config/webpack-stats.js
+++ packages/qiao-webpack/config/webpack-stats.js
@@ -1,14 +1,51 @@
 'use strict';
 
 /**
- * webpack stats options
+ * webpack stats
  */
 module.exports = {
-    assets      : true,
-    assetsSort  : '!size',
-    children    : false,
-    chunks      : false,
-    chunkModules: false,
-    colors      : true,
-    modules     : false,
+    colors              : true,
+
+    // assets
+    assets              : true,
+    assetsSort          : '!size',
+    assetsSpace         : 20,
+    groupAssetsByChunk  : true,
+    
+    // chunks
+    chunks              : false,
+    chunksSort          : 'id',
+    chunkModules        : false,
+    chunkOrigins        : false,
+    chunkGroups         : false,
+    chunkRelations      : false,
+    chunkModulesSpace   : 100,
+
+    // false
+    builtAt             : false,
+    children            : false,
+    depth               : false,
+    entrypoints         : false,
+    cachedAssets        : false,
+    cachedModules       : false,
+    dependentModules    : false,
+    modules             : false,
+    moduleAssets        : false,
+    modulesSort         : 'id',
+    modulesSpace        : 20,
+    nestedModules       : false,
+    nestedModulesSpace  : 20,
+    relatedAssets       : false,
+    runtimeModules      : false,
+    groupAssetsByEmitStatus     : false,
+    groupAssetsByExtension      : false,
+    groupAssetsByInfo           : false,
+    groupAssetsByPath           : false,
+    groupModulesByAttributes    : false,
+    groupModulesByCacheStatus   : false,
+    groupModulesByExtension     : false,
+    groupModulesByLayer         : false,
+    groupModulesByPath          : false,
+    groupModulesByType          : false,
+    groupReasonsByOrigin        : false,
 };
