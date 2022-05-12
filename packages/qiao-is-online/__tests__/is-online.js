fcb1e2dfddb016f0e8e9b75e138a4bd5be6f9616
uikoo9 <uikoo9@qq.com>
2022-05-12 18:18:56 +0800

    feat(qiao-is-online): init

---
+++ packages/qiao-is-online/__tests__/is-online.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var q = require('../index.js');
+
+var test = async function(){
+    var isOnline = await q.isOnline();
+    console.log(isOnline);
+};
+
+test();
---
+++ packages/qiao-is-online/__tests__/offline-to-online.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var q = require('../index.js');
+
+q.offlineToOnline(function(){
+    console.log('offline-to-online');
+}, 3 * 1000);
