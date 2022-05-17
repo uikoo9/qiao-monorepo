37be352134653d2a3191b4f1f5a3b0504b0dea92
uikoo9 <uikoo9@qq.com>
2022-05-17 19:58:45 +0800

    feat(qiao-lerna): add qiao-parallel

--- packages/qiao-lerna/lib/qiao-lerna.js
+++ packages/qiao-lerna/lib/qiao-lerna.js
@@ -9,12 +9,88 @@
  * 	7.最好是多进程执行以上操作
  */
 
-// handler
-var handler = require('./util/handler.js');
+// q
+var q = {};
+q.console 	= require('qiao-console');
+q.process 	= require('qiao-process');
+q.parallel	= require('qiao-parallel');
+
+// fs
+var fs = require('./util/fs.js');
+
+// ncu
+var ncu = require('./util/ncu.js');
+
+// line
+var line = 0;
 
 /**
  * mult ncu
  */
-exports.multiNCU = async function(dir){
-	handler.multiNCU(dir);
+exports.multiNCU = async function(folderName){
+	// clear
+	q.console.clear();
+
+	// start
+	q.console.writeLine(line++, `start operating folder: ${folderName}`);
+
+	// check folder name
+	if(!folderName){
+		q.console.writeLine(line, 'need folder name');
+		return;
+	}
+
+	// dir
+	var dir = fs.path.resolve(process.cwd(), folderName) + fs.path.sep;
+
+	// check dir is folder
+	var isExist = fs.isExists(dir);
+	if(!isExist){
+		q.console.writeLine(line, 'folder is not exists');
+		return;
+	}
+
+	// get sub folders
+	fs.lsdir(dir);
+	if(!fs.subFolders || !fs.subFolders.length){
+		q.console.writeLine(line, 'empty folder');
+		return;
+	}
+
+	// funcs
+	var funcs = [];
+	for(var i=0; i<fs.subFolders.length; i++){
+		funcs.push(handler);
+	}
+	// console.log(funcs);
+	q.parallel.parallelByIIFE(funcs, fs.subFolders, callback, complete);
+
 };
+
+async function handler(folderName){
+	var res 	= await ncu.ncuSubFolders(folderName);
+	var json	= getJson(res);
+	var msg		= `${folderName} : ${json}`;
+
+	return msg;
+}
+
+// get json
+function getJson(s){
+	try{
+		return JSON.stringify(s);
+	}catch(e){
+		return s;
+	}
+}
+
+// callback
+function callback(index, res){
+    q.console.writeLine(line + index, res);
+}
+
+// complete
+function complete(l){
+	q.console.writeLine(line + l, '');
+	q.console.writeLine(line + l + 1, 'multi update npm packages end');
+}
