42fd4929808ab0ba9bdd41f8c9933cee062cfaf1
uikoo9 <uikoo9@qq.com>
2022-05-18 20:16:39 +0800

    feat(qiao-server): init

---
+++ packages/qiao-server/README.md
@@ -0,0 +1,5 @@
+# qiao-server
+
+## version
+### 0.0.1.20220518
+1. init
---
+++ packages/qiao-server/lib/qiao-server.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// express
+var express 		= require('express');
+var bodyParser 		= require('body-parser');
+var cookieParser	= require('cookie-parser');
+
+// require
+var qiao = require('qiao-file');
+
+/**
+ * init
+ * @param {*} options 
+ * @returns 
+ */
+exports.init = function(options){
+    // check
+    if(!options) return;
+
+    // config
+    if(options.config){
+        global.config = options.config;
+    }
+	
+	// app
+	var app = express();
+	app.use(cookieParser());
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended:true}));
+	app.use(bodyParser.text({type:'text/xml'}));
+	
+	// static paths
+    if(options.staticPaths){
+        options.staticPaths.forEach(function(staticPath){
+            app.use(staticPath.name, express.static(staticPath.path, {maxAge: 0}));
+        });
+    }
+
+	// auth
+    if(options.mids){
+        options.mids.forEach(function(mid){
+            app.use(mid);
+        });
+    }
+	
+	// controller
+	var serverFiles = qiao.lsdir(__dirname + '/');
+	for(var i=0; i<serverFiles.files.length; i++){
+		var file = serverFiles.files[i].path + serverFiles.files[i].name;
+		if(/Controller\.js$/.test(file)) require(file)(app);
+	}
+	
+	// inits
+    require('qiao-server-user').init(app);
+    if(options.inits){
+        options.inits.forEach(function(init){
+            init.init(app);
+        });
+    }
+	
+	// port
+	app.listen(global.config.port);
+
+	// return 
+	return app;
+};
