44f50e68211c2ac2e3f626a431b6fb6773574878
uikoo9 <uikoo9@qq.com>
2022-05-18 20:47:12 +0800

    feat(dishi-server): qiao-server

--- packages/dishi-monorepo/packages/dishi-server/_qiao.js
+++
@@ -1,7 +0,0 @@
-// qiao
-var qiao 	= {};
-qiao.encode	= require('qiao-encode');
-qiao.file	= require('qiao-file');
-qiao.json 	= require('qiao-json');
-
-module.exports = qiao;
--- packages/dishi-monorepo/packages/dishi-server/app.js
+++ packages/dishi-monorepo/packages/dishi-server/app.js
@@ -1,50 +1,34 @@
-// require
-var path 	= require('path');
-var qiao 	= require('./_qiao.js');
-
-// global
-global.config 	= require('./config/config.json');
-
-// vars
-var root	= __dirname;
-var port	= global.config.port;
-
-// express
-var express 		= require('express');
-var bodyParser 		= require('body-parser');
-var cookieParser	= require('cookie-parser');
-
-// app
-var app = express();
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.text({type:'text/xml'}));
+'use strict';
 
 // path
-var sslPath		= path.resolve(root, './.well-known');
-var filesPath	= path.resolve(root, './files');
+var path = require('path');
 
-// static
-app.use('/files', express.static(filesPath, {maxAge: 0}));
-app.use('/.well-known', express.static(sslPath, {maxAge: 0}));
+// config
+var config = require('./config/config.json');
 
-// auth
-app.use(require('./middleware/qiao.mid.js').crossDomain);
-app.use(require('./middleware/qiao.mid.js').auth);
+// statics paths
+var staticPaths = [{
+	name : '/files',
+	path : path.resolve(__dirname, './.well-known')
+},{
+	name : '/.well-known',
+	path : path.resolve(__dirname, './files')
+}];
 
-// controller
-var serverFiles = qiao.file.lsdir(root + '/');
-for(var i=0; i<serverFiles.files.length; i++){
-	var file = serverFiles.files[i].path + serverFiles.files[i].name;
-	if(/Controller\.js$/.test(file)) require(file)(app);
-}
+// mids
+var mids = [];
 
-// qiao-server-user
-require('qiao-server-user').init(app);
+// inits
+var inits = [
+	require('dishi-server-todo')
+];
 
-// dishi-server-todo
-require('dishi-server-todo').init(app);
-
-// port
-app.listen(port);
+// qiao-server
+var q = require('qiao-server');
+var options = {
+	config		: config,
+	staticPaths	: staticPaths,
+	mids		: mids,
+	inits		: inits,
+};
+q.init(options);
