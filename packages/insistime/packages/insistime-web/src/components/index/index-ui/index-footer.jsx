e818d6bbd3e8b8eb9cac0074eb6dc63b8ef28e94
uikoo9 <uiloo9@qq.com>
2022-05-19 18:56:07 +0800

    feat(insistime-web): beian

--- packages/insistime/packages/insistime-web/src/components/index/index-ui/index-footer.jsx
+++ packages/insistime/packages/insistime-web/src/components/index/index-ui/index-footer.jsx
@@ -18,7 +18,8 @@ export default class IndexFooter extends React.Component {
                     textAlign: 'center',
                 }}
             >
-                insistime.com ©2022
+				<a target="_blank" href="https://insistime.com/">insistime.com</a>&nbsp;&nbsp;&nbsp;
+				<a target="_blank" href="https://beian.miit.gov.cn/">京ICP备18041615号-1</a>
             </Footer>
         );
     }
