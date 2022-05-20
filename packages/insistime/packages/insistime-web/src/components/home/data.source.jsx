60b5c4245711862218d547fa6734016805cc7bfe
uikoo9 <uiloo9@qq.com>
2022-05-20 17:20:43 +0800

    feat(insistime-web): add home

---
+++ packages/insistime/packages/insistime-web/src/components/home/data.source.jsx
@@ -0,0 +1,330 @@
+import React from 'react';
+export const Nav30DataSource = {
+  wrapper: { className: 'header3 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header3-logo',
+    children:
+      'https://insistime-1252774635.cos.ap-beijing.myqcloud.com/00_insistime/logo_xx.png',
+  },
+  Menu: {
+    className: 'header3-menu',
+    children: [
+      {
+        name: 'item0',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <p>首页</p>
+                </span>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+      {
+        name: 'item1',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <p>案例</p>
+                </span>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+      {
+        name: 'item2',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <p>Github</p>
+                </span>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header3-mobile-menu' },
+};
+export const Banner50DataSource = {
+  wrapper: { className: 'home-page-wrapper banner5' },
+  page: { className: 'home-page banner5-page' },
+  childWrapper: {
+    className: 'banner5-title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <p>insistime.com</p>
+            </span>
+          </span>
+        ),
+        className: 'banner5-title',
+      },
+      {
+        name: 'content',
+        className: 'banner5-content l3e69z913md-editor_css',
+        children: (
+          <span>
+            <p>坚持，时机，创造，热爱</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  image: {
+    className: 'banner5-image',
+    children:
+      'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*-wAhRYnWQscAAAAAAAAAAABkARQnAQ',
+  },
+};
+export const Feature30DataSource = {
+  wrapper: { className: 'home-page-wrapper content3-wrapper' },
+  page: { className: 'home-page content3' },
+  OverPack: { playScale: 0.3 },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>提供专业的服务</p>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+    ],
+  },
+  block: {
+    className: 'content3-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://insistime-1252774635.cos.ap-beijing.myqcloud.com/00_insistime/logo1.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <span>
+                  <p>网页开发</p>
+                </span>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>PC浏览器网页开发</p>
+                <p>
+                  <span>手机H5页面开发</span>
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://insistime-1252774635.cos.ap-beijing.myqcloud.com/00_insistime/logo2.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <span>
+                  <p>微信开发</p>
+                </span>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>微信公众号开发</p>
+                <p>微信小程序开发</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://insistime-1252774635.cos.ap-beijing.myqcloud.com/00_insistime/logo3.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>管理系统开发</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>各种管理系统开发</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://insistime-1252774635.cos.ap-beijing.myqcloud.com/00_insistime/logo4.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>桌面应用开发</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>windows桌面应用开发</p>
+                <p>mac桌面应用开发</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block4',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://insistime-1252774635.cos.ap-beijing.myqcloud.com/00_insistime/logo5.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>APP开发</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>iOS APP开发</p>
+                <p>Android APP开发</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block5',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://insistime-1252774635.cos.ap-beijing.myqcloud.com/00_insistime/logo6.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>其他服务</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>域名注册</p>
+                <p>服务器部署等</p>
+              </span>
+            ),
+          },
+        },
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <p>
+          <span>©2022 insistime.com&nbsp;All Rights Reserved</span>
+        </p>
+      </span>
+    ),
+  },
+};
---
+++ packages/insistime/packages/insistime-web/src/components/home/index.jsx
@@ -0,0 +1,80 @@
+/* eslint no-undef: 0 */
+/* eslint arrow-parens: 0 */
+import React from 'react';
+import { enquireScreen } from 'enquire-js';
+
+import Nav3 from './Nav3.jsx';
+import Banner5 from './Banner5.jsx';
+import Feature3 from './Feature3.jsx';
+import Footer0 from './Footer0.jsx';
+
+import {
+  Nav30DataSource,
+  Banner50DataSource,
+  Feature30DataSource,
+  Footer00DataSource,
+} from './data.source.jsx';
+import './less/antMotionStyle.less';
+
+let isMobile;
+enquireScreen((b) => {
+  isMobile = b;
+});
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile,
+    };
+  }
+
+  componentDidMount() {
+    // 适配手机屏幕;
+    enquireScreen((b) => {
+      this.setState({ isMobile: !!b });
+    });
+  }
+
+  render() {
+    const children = [
+      <Nav3
+        id="Nav3_0"
+        key="Nav3_0"
+        dataSource={Nav30DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Banner5
+        id="Banner5_0"
+        key="Banner5_0"
+        dataSource={Banner50DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature3
+        id="Feature3_0"
+        key="Feature3_0"
+        dataSource={Feature30DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Footer0
+        id="Footer0_0"
+        key="Footer0_0"
+        dataSource={Footer00DataSource}
+        isMobile={this.state.isMobile}
+      />,
+    ];
+    return (
+      <div
+        className="templates-wrapper"
+        ref={(d) => {
+          this.dom = d;
+        }}
+      >
+        {/* 如果不是 dva 2.0 替换成 {children} start */}
+        {/* {this.state.show && children} */}
+        {children}
+        {/* 如果不是 dva 2.0 替换成 {children} end */}
+      </div>
+    );
+  }
+}
