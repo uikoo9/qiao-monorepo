5774ee9767cf9b178095a584667c9d085f52b907
uikoo9 <uiloo9@qq.com>
2022-05-22 22:30:01 +0800

    fix(insistime-server): index to mobile

--- packages/insistime/packages/insistime-server/mvc/service/IndexService.js
+++ packages/insistime/packages/insistime-server/mvc/service/IndexService.js
@@ -9,9 +9,9 @@ var util = require("../util.js");
 exports.index = function (req, res) {
   var vendor = util.vendor(req.headers["user-agent"]);
   if (vendor.mobile || vendor.android) {
-    res.render("index.html");
-  } else {
     res.redirect("/mobile");
+  } else {
+    res.render("index.html");
   }
 };
 
