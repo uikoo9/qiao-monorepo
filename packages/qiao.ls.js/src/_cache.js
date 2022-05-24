211bf1fb1da8f349ef964c91edb5edf23270f218
uikoo9 <uikoo9@qq.com>
2022-05-24 16:29:20 +0800

    feat(qiao.ls.js): side effects

---
+++ packages/qiao.ls.js/src/_cache.js
@@ -0,0 +1,70 @@
+// ls
+import { ls } from "./_ls.js";
+
+// cache
+export const cache = (name, key, value, expires) => {
+  if (!name) return;
+
+  // clear
+  if (key === null) {
+    _clearCache(name);
+    return;
+  }
+
+  // remove
+  if (value === null) {
+    _removeCache(name, key);
+    return;
+  }
+
+  // get
+  if (typeof value == "undefined") {
+    return _getCache(name, key);
+  }
+
+  // set
+  _setCache(name, key, value, expires);
+};
+
+// set cache
+function _setCache(name, key, value, exp) {
+  if (!localStorage) {
+    console.log("unsupport localStorage");
+    return;
+  }
+
+  if (!name || !key) return;
+
+  let data = ls(name) || {};
+  data[key] = value;
+
+  ls(name, data, exp || 7 * 24 * 60 * 60 * 1000);
+}
+
+// get cache
+function _getCache(name, key) {
+  if (!name || !key) return;
+
+  const data = ls(name);
+  if (!data) return;
+
+  return data[key];
+}
+
+// remove cache
+function _removeCache(name, key) {
+  if (!name || !key) return;
+
+  let data = ls(name);
+  if (!data) return;
+
+  delete data[key];
+  ls(name, data);
+}
+
+// clear cache
+function _clearCache(name) {
+  if (!name) return;
+
+  ls(name, null);
+}
--- packages/qiao.ls.js/src/_ls.js
+++ packages/qiao.ls.js/src/_ls.js
@@ -1,7 +1,3 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
 /**
  * @function ls
  * @param {string} name key
@@ -20,7 +16,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
  * // del localstorage item by key: name
  * ls(name, null):
  */
-const ls = (name, value, expires) => {
+export const ls = (name, value, expires) => {
   // remove
   if (value === null) {
     _removeItem(name);
@@ -84,76 +80,3 @@ function _removeItem(name) {
 
   localStorage.removeItem(name);
 }
-
-// ls
-
-// cache
-const cache = (name, key, value, expires) => {
-  if (!name) return;
-
-  // clear
-  if (key === null) {
-    _clearCache(name);
-    return;
-  }
-
-  // remove
-  if (value === null) {
-    _removeCache(name, key);
-    return;
-  }
-
-  // get
-  if (typeof value == "undefined") {
-    return _getCache(name, key);
-  }
-
-  // set
-  _setCache(name, key, value, expires);
-};
-
-// set cache
-function _setCache(name, key, value, exp) {
-  if (!localStorage) {
-    console.log("unsupport localStorage");
-    return;
-  }
-
-  if (!name || !key) return;
-
-  let data = ls(name) || {};
-  data[key] = value;
-
-  ls(name, data, exp || 7 * 24 * 60 * 60 * 1000);
-}
-
-// get cache
-function _getCache(name, key) {
-  if (!name || !key) return;
-
-  const data = ls(name);
-  if (!data) return;
-
-  return data[key];
-}
-
-// remove cache
-function _removeCache(name, key) {
-  if (!name || !key) return;
-
-  let data = ls(name);
-  if (!data) return;
-
-  delete data[key];
-  ls(name, data);
-}
-
-// clear cache
-function _clearCache(name) {
-  if (!name) return;
-
-  ls(name, null);
-}
-
-exports.cache = cache;
-exports.ls = ls;
