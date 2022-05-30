a5bc38c94abe3cc8be69ae67af47cb0290d70ae2
uikoo9 <uikoo9@qq.com>
2022-05-30 22:28:29 +0800

    feat(qiao-server): build

--- packages/qiao-server/index.js
+++ packages/qiao-server/index.js
@@ -42,8 +42,10 @@ var initStatic = (app, options) => {
   app.use("/.well-known", express.static(acmePath, { maxAge: 0 }));
 
   // static
-  const staticPath = path.resolve(process.cwd(), "./web/static");
-  app.use("/static", express.static(staticPath, { maxAge: 0 }));
+  if(options.isDev){
+    const staticPath = path.resolve(process.cwd(), "./web/static");
+    app.use("/static", express.static(staticPath, { maxAge: 0 }));
+  }
 
   // options
   if (options.staticPaths) {
