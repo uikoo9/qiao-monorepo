12aabcb28b855d3a97d3abb28a70e917c4b0c7f3
uikoo9 <uikoo9@qq.com>
2022-05-30 22:24:49 +0800

    feat(qiao-server): is dev static

--- packages/qiao-server/src/init/init-static.js
+++ packages/qiao-server/src/init/init-static.js
@@ -15,8 +15,10 @@ export default (app, options) => {
   app.use("/.well-known", express.static(acmePath, { maxAge: 0 }));
 
   // static
-  const staticPath = path.resolve(process.cwd(), "./web/static");
-  app.use("/static", express.static(staticPath, { maxAge: 0 }));
+  if(options.isDev){
+    const staticPath = path.resolve(process.cwd(), "./web/static");
+    app.use("/static", express.static(staticPath, { maxAge: 0 }));
+  }
 
   // options
   if (options.staticPaths) {
