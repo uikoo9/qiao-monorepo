e8cd888317ad9da182fcd76fc87cf14edf6376f9
uikoo9 <uikoo9@qq.com>
2022-06-15 10:38:12 +0800

    feat(qiao-server): auth add cookies

--- packages/qiao-server/src/init/_mids.js
+++ packages/qiao-server/src/init/_mids.js
@@ -1,10 +1,10 @@
 'use strict';
 
 // qiao
-import { 
-	encode as qencode,
-	json as qjson,
-	user as quser,
+import {
+    encode as qencode,
+    json as qjson,
+    user as quser,
 } from '../_qiao.js';
 
 /**
@@ -14,10 +14,10 @@ import {
  * @param {*} next 
  */
 export const crossDomain = (req, res, next) => {
-	res.set('Access-Control-Allow-Origin', '*');
-	res.set('Access-Control-Allow-Methods', '*');
-	res.set('Access-Control-Allow-Headers', 'Content-Type');
-	next();
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', '*');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 };
 
 /**
@@ -28,55 +28,55 @@ export const crossDomain = (req, res, next) => {
  * @returns 
  */
 export const auth = async (req, res, next) => {
-	// path
+    // path
     const path = req.path;
 
-	// auth - normal visit
-	let normalVisit		= false;
+    // auth - normal visit
+    let normalVisit = false;
     const normalVisitPath = global.config.paths;
-	for(let i=0; i<normalVisitPath.length; i++){
-		if(path == normalVisitPath[i]) normalVisit = true;
-	}
-	if(normalVisit){
-		next();
-		return;
-	}
-
-	// auth - has token
-	const userid	= req.headers.userid;
-	const usertoken	= req.headers.usertoken;
-	if(!userid || !usertoken){
-		res.send(qjson.danger('缺少token！'));
-		return;
+    for (let i = 0; i < normalVisitPath.length; i++) {
+        if (path == normalVisitPath[i]) normalVisit = true;
     }
-	
-	// auth - check token
-	try{
-		// get user
-		const rows = await quser.ucenterUserModel.ucenterUserGetById(userid);
-		if(!rows || rows.length != 1){
-			res.send(qjson.danger('缺少用户信息！'));
-			return;
-		}
-		
-		// check token
-		const user 		= rows[0];
-		const username	= user['ucenter_user_name'];
-		const password	= user['ucenter_user_password'];
-		const rUsertoken= qencode.AESEncrypt(username + password, global.config.encryptKey);
-		
-		// send
-		if(usertoken != rUsertoken){
-			res.send(qjson.danger('非法token！'));
-			return;
-		}
-		
-		// set userinfo
-		req.body['express_userid'] 	= userid;
-		req.body['express_username']= username;
-		
-		next();
-	}catch(e){
-		res.send(qjson.danger('校验token失败！', {errName:e.name,errMsg:e.message}));
-	}
+    if (normalVisit) {
+        next();
+        return;
+    }
+
+    // auth - has token
+    const userid = req.headers.userid || req.cookies.userid;
+    const usertoken = req.headers.usertoken || req.cookies.usertoken;
+    if (!userid || !usertoken) {
+        res.send(qjson.danger('缺少token！'));
+        return;
+    }
+
+    // auth - check token
+    try {
+        // get user
+        const rows = await quser.ucenterUserModel.ucenterUserGetById(userid);
+        if (!rows || rows.length != 1) {
+            res.send(qjson.danger('缺少用户信息！'));
+            return;
+        }
+
+        // check token
+        const user = rows[0];
+        const username = user['ucenter_user_name'];
+        const password = user['ucenter_user_password'];
+        const rUsertoken = qencode.AESEncrypt(username + password, global.config.encryptKey);
+
+        // send
+        if (usertoken != rUsertoken) {
+            res.send(qjson.danger('非法token！'));
+            return;
+        }
+
+        // set userinfo
+        req.body['express_userid'] = userid;
+        req.body['express_username'] = username;
+
+        next();
+    } catch (e) {
+        res.send(qjson.danger('校验token失败！', { errName: e.name, errMsg: e.message }));
+    }
 };
