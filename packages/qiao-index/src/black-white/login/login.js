cc2166816905b5afb65249c2e76070a2eb86dac6
uikoo9 <uikoo9@qq.com>
2022-06-16 14:50:09 +0800

    feat(insistime-web): login cookie

--- packages/qiao-index/src/black-white/login/login.js
+++ packages/qiao-index/src/black-white/login/login.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// cookie
-import Cookies from 'js-cookie';
+// qiao
+import qiao from 'qiao.cookie.js';
 
 // qiao
 import { login } from 'qiao-service';
@@ -34,7 +34,7 @@ export const loginBtnClick = async (username, password, callback, loginSucUrl) =
 
     // suc
     callback(res.msg);
-    Cookies.set('userid', res.obj.userid);
-    Cookies.set('usertoken', res.obj.usertoken);
+    qiao.set('insistime_userid', res.obj.userid);
+    qiao.set('insistime_usertoken', res.obj.usertoken);
     location.href = loginSucUrl;
 };
