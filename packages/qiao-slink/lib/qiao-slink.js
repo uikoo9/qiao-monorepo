60c00f538d9c9ec0bcced505cf8ee45809aecbf2
uikoo9 <uikoo9@qq.com>
2022-06-25 10:40:16 +0800

    fix(qiao-slink): encode

--- packages/qiao-slink/lib/qiao-slink.js
+++ packages/qiao-slink/lib/qiao-slink.js
@@ -1,35 +1,46 @@
 'use strict';
 
+// qs
+var qs = require('qs');
+
 // qiao-ajax
 var q = require('qiao-ajax');
 
 // vars
-var turl 	= 'https://tiyee.cn/2/create_short_url';
-var noUrl	= new Error('no url');
-var resErr	= new Error('res error');
+var turl = 'https://tiyee.cn/2/create_short_url';
+var noUrl = new Error('no url');
+var resErr = new Error('res error');
 
 /**
  * short link
  */
-exports.shortLink = function(longLink){
-	return new Promise(function(resolve, reject){
-		if(!longLink) return reject(noUrl);
+exports.shortLink = function (longLink) {
+    return new Promise(function (resolve, reject) {
+        if (!longLink) return reject(noUrl);
 
-		// config
-		var config = {data : `url=${longLink}`};
+        // data
+        var data = {
+            url: longLink
+        };
 
-		// post
-		q.post(turl, config)
-			.then(function(res){
-				// not 200
-				if(!res || res.status != 200 || !res.data){
-					return reject(resErr);
-				}
+        // config
+        var config = {
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            data: qs.stringify(data)
+        };
 
-				return resolve(res.data.short_url);
-			})
-			.catch(function(e){
-				reject(e);
-			});
-	});
+        // post
+        q.post(turl, config)
+            .then(function (res) {
+                // not 200
+                if (!res || res.status != 200 || !res.data) {
+                    return reject(resErr);
+                }
+
+                return resolve(res.data.short_url);
+            })
+            .catch(function (e) {
+                reject(e);
+            });
+    });
 };
