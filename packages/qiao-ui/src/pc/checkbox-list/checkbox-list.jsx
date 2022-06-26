cc9c58886cffc36435404fa0f6f5671ac46062c8
uikoo9 <uikoo9@qq.com>
2022-06-26 09:36:21 +0800

    feat(qiao-ui): checkbox list default checked

--- packages/qiao-ui/src/pc/checkbox-list/checkbox-list.jsx
+++ packages/qiao-ui/src/pc/checkbox-list/checkbox-list.jsx
@@ -16,33 +16,43 @@ export class CheckboxList extends React.Component {
         this.checkboxChange = this.checkboxChange.bind(this);
     }
 
-    checkboxChange(e){
-        if(!this.props.checkboxChange) return;
-        
+    checkboxChange(e) {
+        if (!this.props.checkboxChange) return;
+
         this.props.checkboxChange(e.target.checked, e.target.value);
     }
-    
+
     render() {
         const checkboxs = this.props.checkboxs && this.props.checkboxs.map((ck, index) => {
             if (!ck.name) return;
 
+            const inputNormal = <input
+                type="checkbox"
+                id={ck.id || ck.name}
+                name={ck.name}
+                value={ck.value || ck.name}
+                onChange={this.checkboxChange}
+            />;
+            const inputChecked = <input
+                type="checkbox"
+                id={ck.id || ck.name}
+                name={ck.name}
+                value={ck.value || ck.name}
+                onChange={this.checkboxChange}
+                defaultChecked
+            />;
+
             return <div className="item" key={index}>
-                <input 
-                    type="checkbox" 
-                    id={ck.id || ck.name} 
-                    name={ck.name} 
-                    value={ck.value || ck.name} 
-                    onChange={this.checkboxChange}
-                />
-                <label htmlFor="express">{ck.name}</label>
+                {ck.checked ? inputChecked : inputNormal}
+                <label htmlFor={ck.id || ck.name}>{ck.name}</label>
             </div>
         });
-        
+
         return <div className="checkbox-list">
             {
                 this.props.text ? <div className="item">{this.props.text}</div> : null
             }
-            { checkboxs }
+            {checkboxs}
         </div>;
     }
 }
