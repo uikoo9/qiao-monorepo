5bc629293fd3bfa7dd32302ea81a0035bbdad524
uikoo9 <uikoo9@qq.com>
2022-08-01 20:13:04 +0800

    feat(qiao-ui): mobile info list --> cards

--- packages/qiao-ui/src/mobile/index.js
+++ packages/qiao-ui/src/mobile/index.js
@@ -1,5 +1,5 @@
 // mobile
 export { MobileHeader } from './header/header.jsx';
 export { MobileContent } from './content/content.jsx';
-export { MobileInfoList } from './info-list/info-list.jsx';
+export { MobileCards } from './cards/cards.jsx';
 export { MobileFooter } from './footer/footer.jsx';
