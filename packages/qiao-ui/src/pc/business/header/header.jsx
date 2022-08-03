ae510a804b58e060fd68213f179110910eaf447d
uikoo9 <uikoo9@qq.com>
2022-08-03 16:13:45 +0800

    feat: header hook

--- packages/qiao-ui/src/pc/business/header/header.jsx
+++ packages/qiao-ui/src/pc/business/header/header.jsx
@@ -10,25 +10,23 @@ import { Link } from '../../index.js';
 /**
  * header
  */
-export class Header extends React.Component {
-    render() {
-        console.log('qiao-ui/pc/header: render');
+export function Header(props) {
+    console.log('qiao-ui/pc/header: render');
 
-        const navs = this.props.navs && this.props.navs.map((nav, index) => {
-            if (!nav.url || !nav.name) return;
+    const navs = props.navs && props.navs.map((nav, index) => {
+        if (!nav.url || !nav.name) return;
 
-            return <div className="nav nav-txt right" key={index}>
-                <Link blank={nav.blank} url={nav.url} txt={nav.name} />
+        return <div className="nav nav-txt right" key={index}>
+            <Link blank={nav.blank} url={nav.url} txt={nav.name} />
+        </div>
+    });
+
+    return (
+        <div className="header">
+            <div className="logo nav-txt">
+                <Link url={props.logoUrl} txt={props.logo} />
             </div>
-        });
-
-        return (
-            <div className="header">
-                <div className="logo nav-txt">
-                    <Link url={this.props.logoUrl} txt={this.props.logo} />
-                </div>
-                <div className="navs">{navs}</div>
-            </div>
-        );
-    }
+            <div className="navs">{navs}</div>
+        </div>
+    );
 }
