804292fe612b3def497f4176d336288455566c0e
uikoo9 <uikoo9@qq.com>
2022-08-03 14:50:47 +0800

    feat(qiao-ui): del qiao-ui

--- packages/qiao-ui/src/index.js
+++
@@ -1,5 +0,0 @@
-// pc
-export * from './pc/index.js';
-
-// mobile
-export * from './mobile/index.js';
--- packages/qiao-ui/src/mobile/index.js
+++
@@ -1,5 +0,0 @@
-// mobile
-export { MobileHeader } from './header/header.jsx';
-export { MobileContent } from './content/content.jsx';
-export { MobileCards } from './cards/cards.jsx';
-export { MobileFooter } from './footer/footer.jsx';
