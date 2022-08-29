f000b300b4a441b4b64233cdaacf1dd38e461c01
uikoo9 <uikoo9@qq.com>
2022-08-29 19:26:33 +0800

    feat(mobile): info list 1

---
+++ packages/qiao-ui/src/mobile/components/info/info.jsx
@@ -0,0 +1,26 @@
+// react
+import React from 'react';
+
+// css
+import './info.scss';
+
+// ui
+import { Link } from '../../index.js';
+
+// log
+import { colorLog } from '../../../util/log.js';
+
+/**
+ * info
+ */
+export const Info = (props) => {
+    colorLog('qiao-ui/pc/info: render');
+
+    return <div className="info">
+        <div className="item-title">
+            <Link blank={props.blank} url={props.url} txt={props.title} />
+        </div>
+        <div className="item-desc">{props.desc}</div>
+        <div className="item-other">{props.other}</div>
+    </div>;
+};
