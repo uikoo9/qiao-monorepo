fa521bd73dd4310297ddea0ce9c58d7edc073df5
uikoo9 <uikoo9@qq.com>
2022-08-29 20:15:51 +0800

    feat(mobile): article ok

---
+++ packages/qiao-ui/src/mobile/components/article/article.jsx
@@ -0,0 +1,35 @@
+// react
+import React from 'react';
+
+// css
+import './article.scss';
+
+// log
+import { colorLog } from '../../../util/log.js';
+
+/**
+ * mobile article
+ */
+export const MobileArticle = (props) => {
+    colorLog('qiao-ui/mobile/article: render');
+
+    return (
+        <div className="article-container">
+            {
+                props.title ? <div className="article-title">{props.title}</div> : null
+            }
+            {
+                props.date ? <div className="article-date">{props.date}</div> : null
+            }
+            {
+                props.times ? <div className="article-times">阅读{props.times}次</div> : null
+            }
+            {
+                props.content ? <div className="article-content" dangerouslySetInnerHTML={{ __html: props.content }} /> : null
+            }
+            {
+                props.children
+            }
+        </div>
+    );
+};
--- packages/qiao-ui/src/mobile/index.js
+++ packages/qiao-ui/src/mobile/index.js
@@ -15,3 +15,4 @@ export { MobileFooter } from './components/footer/footer.jsx';
 // blog
 export { MobileInfo } from './components/info/info.jsx';
 export { MobileInfoList } from './components/info-list/info-list.jsx';
+export { MobileArticle } from './components/article/article.jsx';
