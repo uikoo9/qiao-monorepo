0f4daccce1e6a2c7acaf56903cba7b6948cca35b
uikoo9 <uikoo9@qq.com>
2022-08-29 19:52:46 +0800

    docs: version

--- packages/qiao-ui/src/mobile/index.js
+++ packages/qiao-ui/src/mobile/index.js
@@ -1,17 +1,17 @@
 /**
  * basic
  */
- export { MobileBreadcrumbs } from './basic/breadcrumbs/breadcrumbs.jsx';
+export { MobileBreadcrumbs } from './basic/breadcrumbs/breadcrumbs.jsx';
 
- /**
+/**
   * components
   */
- // index
- export { MobileHeader } from './components/header/header.jsx';
- export { MobileContent } from './components/content/content.jsx';
- export { MobileCards } from './components/cards/cards.jsx';
- export { MobileFooter } from './components/footer/footer.jsx';
+// index
+export { MobileHeader } from './components/header/header.jsx';
+export { MobileContent } from './components/content/content.jsx';
+export { MobileCards } from './components/cards/cards.jsx';
+export { MobileFooter } from './components/footer/footer.jsx';
  
- // blog
- export { MobileInfo } from './components/info/info.jsx';
- export { MobileInfoList } from './components/info-list/info-list.jsx';
+// blog
+export { MobileInfo } from './components/info/info.jsx';
+export { MobileInfoList } from './components/info-list/info-list.jsx';
