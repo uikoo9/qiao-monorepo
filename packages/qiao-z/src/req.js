aa5b669f89728719c48fbc00e283557a32af7736
uikoo9 <uikoo9@qq.com>
2022-10-09 11:14:23 +0800

    feat(qiao-z): headers

--- packages/qiao-z/src/req.js
+++ packages/qiao-z/src/req.js
@@ -10,6 +10,22 @@ module.exports = function(request){
     const req = {};
     req.request = request;
     req.url = parseurl(request);
+    req.headers = handleHeaders(request);
 
     return req;
 };
+
+// handle headers
+function handleHeaders(request){
+    const headers = {};
+
+    // check
+    const rawHeaders = request.rawHeaders;
+    if(!rawHeaders || !rawHeaders.length) return headers;
+
+    // handle
+    rawHeaders.forEach((h, i) => {
+        if(i % 2 == 0) headers[h] = rawHeaders[i+1];
+    });
+    return headers;
+}
