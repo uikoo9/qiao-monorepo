2dd339569c893620fbb25105bc98c43e4eb20763
uikoo9 <uikoo9@qq.com>
2022-10-10 12:41:05 +0800

    fix(qiao-z): req.headers to lower case

--- packages/qiao-z/src/req.js
+++ packages/qiao-z/src/req.js
@@ -25,7 +25,7 @@ function handleHeaders(request){
 
     // handle
     rawHeaders.forEach((h, i) => {
-        if(i % 2 == 0) headers[h] = rawHeaders[i+1];
+        if(i % 2 == 0) headers[h.toLowerCase()] = rawHeaders[i+1];
     });
     return headers;
 }
