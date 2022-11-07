1e9adafc9907379fede80b7d9b359bd2df8c8b2b
uikoo9 <uikoo9@qq.com>
2022-11-07 16:40:46 +0800

    chore: del qiao-z

--- packages/qiao-ua/__tests__/useragent.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var ua = require('../index.js');
-
-var useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36';
-var res = ua(useragent);
-console.log(res);
