b6bed48e1ac431eedcf5c47245a448442d28e3e6
uikoo9 <uikoo9@qq.com>
2022-12-06 10:31:17 +0800

    chore: del qiao-sqlite

--- packages/qiao-sqlite/__tests__/create-db.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// q
-var q = require('../index.js');
-
-// db
-var db = q.createDB('./__tests__/test.db');
-console.log(db);
--- packages/qiao-sqlite/src/index.js
+++
@@ -1,8 +0,0 @@
-// db
-export * from './sqlite-db.js';
-
-// table
-export * from './sqlite-table.js';
-
-// data
-export * from './sqlite-data.js';
