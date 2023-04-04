15527873cf1e1c12fd393d6124c712aa53153390
uikoo9 <uikoo9@qq.com>
2023-04-04 16:51:33 +0800

    feat(qiao-file): mv to qiao-nodejs

--- packages/qiao-file/src/index.js
+++
@@ -1,9 +0,0 @@
-// fs & path
-export * as fs from 'fs-extra';
-export * as path from 'path';
-
-// xx
-export * from './cmd.js';
-export * from './dir.js';
-export * from './file.js';
-export * from './is.js';
