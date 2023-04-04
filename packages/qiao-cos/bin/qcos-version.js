b781c28d0f570fb459811c6fc90f5c9992c603ae
uikoo9 <uikoo9@qq.com>
2023-04-04 20:05:45 +0800

    feat(qiao-cos): mv to qiao-nodejs

--- packages/qiao-cos/bin/qcos-version.js
+++
@@ -1,8 +0,0 @@
-// qiao
-const cli = require('qiao-cli');
-
-// cmd for common
-cli.cmd
-  .version(require('../package.json').version, '-v, --version')
-  .description('qiao-cos, tencent cos upload tool on nodejs')
-  .usage('<command> [options]');
